efebb9945b845fa94d2ad3ff7a3af1e8d7fa3c1f
emilior-octo <emilior@octopusdigitalagency.com>
2026-03-21 15:45:13 +0100

    Fix broken product query file

--- app/lib/graphql/product-for-base-price.ts
+++ app/lib/graphql/product-for-base-price.ts
@@ -9,23 +9,6 @@ export const PRODUCT_FOR_BASE_PRICE_QUERY = `#graphql
         value
         type
       }
-      variants(first: 10) {
-        nodes {
-          id
-          title
-          price
-          compareAtPrice
-          positionexport const PRODUCT_FOR_BASE_PRICE_QUERY = `#graphql
-  query ProductForBasePrice($id: ID!) {
-    product(id: $id) {
-      id
-      title
-      metafield(namespace: "pricing", key: "base_price") {
-        namespace
-        key
-        value
-        type
-      }
       metafield(namespace: "pricing", key: "discount_percentage") {
         namespace
         key
@@ -39,11 +22,6 @@ export const PRODUCT_FOR_BASE_PRICE_QUERY = `#graphql
           price
           compareAtPrice
           position
-        }
-      }
-    }
-  }
-`;
         }
       }
     }
